667f9ab67f2973c1aafe9de77a08a6298e7bf2d9
jianggn <34902969+jianggn@users.noreply.github.com>
2023-05-29 00:25:29 +0800

    Update World.cpp
    双天赋

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -571,6 +571,9 @@ void World::LoadConfigSettings(bool reload)
     setConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_WHO_LIST,            "AllowTwoSide.WhoList", false);
     setConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_ADD_FRIEND,          "AllowTwoSide.AddFriend", false);
 
+    //Dual Talent Specialization
+    setConfig(CONFIG_SWAP_SPEC_INTERVAL,    "SwapSpecInterval", 900);
+
     setConfig(CONFIG_UINT32_STRICT_PLAYER_NAMES,  "StrictPlayerNames",  0);
     setConfig(CONFIG_UINT32_STRICT_CHARTER_NAMES, "StrictCharterNames", 0);
     setConfig(CONFIG_UINT32_STRICT_PET_NAMES,     "StrictPetNames",     0);
