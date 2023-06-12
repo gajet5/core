f96e94456cce7ffc575e4e68473f1138fe968ffc
jianggn <34902969+jianggn@users.noreply.github.com>
2023-06-12 23:21:54 +0800

    Update PartyBotAI.cpp

--- src/game/PlayerBots/PartyBotAI.cpp
+++ src/game/PlayerBots/PartyBotAI.cpp
@@ -2273,8 +2273,8 @@ void PartyBotAI::UpdateInCombatAI_Warlock()
         }
 
         if (m_spells.warlock.pLifeTap &&
-           (me->GetPowerPercent(POWER_MANA) < 10.0f) &&
-           (me->GetHealthPercent() > 70.0f) &&
+           (me->GetPowerPercent(POWER_MANA) < 25.0f) &&
+           (me->GetHealthPercent() > 50.0f) &&
             CanTryToCastSpell(me, m_spells.warlock.pLifeTap))
         {
             if (DoCastSpell(me, m_spells.warlock.pLifeTap) == SPELL_CAST_OK)
