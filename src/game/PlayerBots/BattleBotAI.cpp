be9487942b981db44c9b486d1e5bfa4d9ae36fa4
jianggn <34902969+jianggn@users.noreply.github.com>
2023-06-12 23:24:27 +0800

    Update BattleBotAI.cpp

--- src/game/PlayerBots/BattleBotAI.cpp
+++ src/game/PlayerBots/BattleBotAI.cpp
@@ -2209,8 +2209,8 @@ void BattleBotAI::UpdateInCombatAI_Warlock()
         }
 
         if (m_spells.warlock.pLifeTap &&
-           (me->GetPowerPercent(POWER_MANA) < 10.0f) &&
-           (me->GetHealthPercent() > 70.0f) &&
+           (me->GetPowerPercent(POWER_MANA) < 25.0f) &&
+           (me->GetHealthPercent() > 50.0f) &&
             CanTryToCastSpell(me, m_spells.warlock.pLifeTap))
         {
             if (DoCastSpell(me, m_spells.warlock.pLifeTap) == SPELL_CAST_OK)
