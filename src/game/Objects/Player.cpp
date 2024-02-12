a2d77451f868005ddb79d17b52cf278009eaff40
jianggn <34902969+jianggn@users.noreply.github.com>
2024-02-12 09:26:46 +0800

    20240212 机器人不损失装备耐久

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -5308,6 +5308,10 @@ void Player::DurabilityPointsLossAll(int32 points, bool inventory)
 
 void Player::DurabilityPointsLoss(Item* item, int32 points)
 {
+    //bot do not loss durabilitypoints
+    if (IsBot())
+        return;
+
     int32 pMaxDurability = item->GetUInt32Value(ITEM_FIELD_MAXDURABILITY);
     int32 pOldDurability = item->GetUInt32Value(ITEM_FIELD_DURABILITY);
     int32 pNewDurability = pOldDurability - points;
