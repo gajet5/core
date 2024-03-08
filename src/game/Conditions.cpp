1a5be88b1bc046a0928c99cd854a9d279b11ed19
jianggn <34902969+jianggn@users.noreply.github.com>
2024-03-09 01:04:14 +0800

    1

--- src/game/Conditions.cpp
+++ src/game/Conditions.cpp
@@ -241,7 +241,7 @@ bool inline ConditionEntry::Evaluate(WorldObject const* target, Map const* map, 
         }
         case CONDITION_ACTIVE_GAME_EVENT:
         {
-            if (conditionSourceToStr[conditionSourceType] = "areatrigger" && m_value1 = 83 && sWorld.getConfig(CONFIG_BOOL_INSTANCE_IGNORE_AQ_GATE))
+            if (sWorld.getConfig(CONFIG_BOOL_INSTANCE_IGNORE_AQ_GATE) && (conditionSourceToStr[conditionSourceType] == "areatrigger") && (m_value1 == 83))
                 return true;
             return sGameEventMgr.IsActiveEvent(m_value1);
         }
