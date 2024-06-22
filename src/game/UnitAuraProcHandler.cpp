523bd0f99b86d9f8fd6b59fd0ba581247faf6808
jianggn <34902969+jianggn@users.noreply.github.com>
2024-06-22 11:36:23 +0800

    20240622 熊灵-回音击

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -781,6 +781,16 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit* pVictim, uint32 amount, uint
                     target = this;
                     break;
                 }
+                // Misha: echo slam
+                case 34195:
+                {
+                    // echo slam target count
+                    uint8 EchoSlamTargetCount = this->GetEnemyCountInRadiusAround(this, 10.0f);
+                    basepoints[0] = dither(triggerAmount * EchoSlamTargetCount + 30.0f);
+                    target = this;
+                    triggered_spell_id = 34196;
+                    break;                               // no hidden cooldown
+                }
                 // Sven: mask of madness
                 case 34193:
                 {
